e93f7c14f6d1b67ddb72362c757d3aff18ee3f72
Zorro500 <compremont555@gmail.com>
2026-02-09 19:23:57 +0300

    Fix sound paths

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,6 @@ export default function Footer(){
     return(
         <footer className="footer">
             <p>Разработчик : Мхитарян Эрик Ишханович</p>
-            <p>Претендует на зарплату больше 300 000 р</p>
         </footer>
     )
 }
